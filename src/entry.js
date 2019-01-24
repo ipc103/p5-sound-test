2498ebee76d41643ed73e17280984238af3ca88f
Ian Candy <iancandysemail@gmail.com>
2019-01-24 17:57:24 -0500

    first draft of playing sounds

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,39 @@
-console.log("Hi")
+import 'p5/lib/addons/p5.sound';
+import p5, { MonoSynth } from 'p5'
+
+
+const run = (sk) => {
+  const monoSynth = new p5.MonoSynth();
+  const notes = []
+  for (var i = "A".charCodeAt(); i <= "G".charCodeAt(); i++) {
+    notes.push(String.fromCharCode(i))
+  }
+  let playing = false;
+
+  sk.setup = () => {
+    const canvas = sk.createCanvas(window.innerWidth, window.innerHeight);
+  }
+
+  sk.draw = () => {
+    sk.background(sk.mouseX / 2, sk.mouseY /2);
+
+    if (playing) {
+      const pitch = Math.round(sk.mouseX / 100)
+      const noteIndex = Math.round( (sk.mouseY / window.innerHeight) * notes.length)
+      const note = notes[noteIndex]
+      console.log(note)
+      const tone = note + pitch
+      monoSynth.triggerAttack(tone);
+    }
+  }
+
+  sk.mousePressed = () => {
+    playing = !playing
+  }
+
+  sk.mouseReleased = () => {
+    monoSynth.triggerRelease();
+  }
+}
+
+const P5 = new p5(run);
